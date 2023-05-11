43d268a5bba9bab943db06d194a620180ef90c48
Yelynn Oh <yoyelynn@gmail.com>
2023-05-11 21:43:57 +0900

    [feat] Header 컴포넌트 생성

--- next-netflix/app/components/Home/Header.tsx
+++ next-netflix/app/components/Home/Header.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import styled from "styled-components"
+import Image from "next/image"
+
+export default function Header() {
     return (
-      <></>
+      <Wrapper>
+        <Top>
+            <HeaderNavigator>
+                <Image src="/image/netflix.png" alt="" width={"57"} height={"57"}/>
+                <HeaderText>TV Shows</HeaderText> 
+                <HeaderText>TV Shows</HeaderText> 
+                <HeaderText>TV Shows</HeaderText> 
+            </HeaderNavigator>
+            {/* 랜덤 함수 */}
+        </Top>
+
+      </Wrapper>
     )
   }
+
+const Wrapper = styled.div`
+
+`;
+
+const Top = styled.div`
+
+`;
+
+const HeaderNavigator = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 375px;   
+  padding: 1rem 1rem 0 0;
+  cursor: pointer;
+`;
+
+const HeaderText = styled.div`
+  font-size: 1.1rem;
+  font-weight: 400;
+  color: white;
+`;
+
+const Logo = styled.div`
+
+`;
