c1c494eb5484048c993f49c8ed6ee2ecc8bcbe5c
hmuri <113418916+hmuri@users.noreply.github.com>
2023-05-11 14:17:31 +0900

    [feat] 라우팅 및 MainPage 생성

---
+++ next-netflix/app/MainPage/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width : 50%;
+    margin : auto;
+`
+
+function Logo(){
+    
+    return (
+        <Container>main</Container>
+
+    );
+}
+
+export default Logo;
