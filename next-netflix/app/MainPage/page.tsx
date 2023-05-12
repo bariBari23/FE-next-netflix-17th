8b35f568d92a76268b84196ee60709f70c65e4d4
Yelynn Oh <yoyelynn@gmail.com>
2023-05-12 10:14:18 +0900

    [style] 전체 home 화면 스타일 수정

--- next-netflix/app/MainPage/page.tsx
+++ next-netflix/app/MainPage/page.tsx
@@ -17,11 +17,9 @@ export default function Home() {
   HandleRecoil();
 
   return (
-    // 지금 div는 home.module.css 파일의 .container style을 따르고 있음! 
-    // 거기 보면 height:100%인데 아직 아무 컴포넌트의 height도 추가되지 않아서 그냥 배경이 까맣게 보일거야! 여긴 그냥 컴포넌트 쭉 쓰고 component 폴더에 만들어도 될 것 같아!
+    
     <div className={styles.container}>
-      <Container>
-          <Header></Header>
+                <Header></Header>
           <TitleText style={{ fontSize: '26.75px' }}>Previews</TitleText>
           <MovieSlider>
             {
@@ -43,18 +41,18 @@ export default function Home() {
                     },
             )}
         </MovieSlider>
-      </Container>  
+
 
     </div>
   )
 }
 
-const Container = styled.div`
-  width : 375px;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-`
+// const Container = styled.div`
+//   width : 375px;
+//   height: 100%;
+//   display: flex;
+//   flex-direction: column;
+// `
 const MovieSlider = styled.div`
     display: flex;
     width: 370px;
